79a6911e1cbefc14dfb375698455cd682bba4f97
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2023-09-24 01:11:25 -0300

    Add files via upload

---
+++ 2015/day10/puzzle2.md
@@ -0,0 +1,10 @@
+## --- Part Two ---
+
+Neat, right? You might also enjoy hearing
+[John Conway talking about this sequence](https://www.youtube.com/watch?v=ea7lJkEhytA)
+(that's Conway of **Conway's Game of Life** fame).
+
+Now, starting again with the digits in your puzzle input, apply this process
+**50** times. What is **the length of the new result**?
+
+Your puzzle input is still `1113222113`.
---
+++ 2015/day10/solve1.js
@@ -0,0 +1,48 @@
+"use strict"
+
+// solving the puzzle takes (my computer) 0.017s
+
+let string = ""
+
+function main() {
+
+    string = Deno.readTextFileSync("input.txt").trim()
+    
+    for (let n = 0; n < 40; n++) { processString() }    
+
+    console.log("result length is", string.length)
+}
+
+function processString() {
+
+    let newString = ""
+
+    let group = ""
+    
+    while (true) {
+    
+        const c = string[0]
+        
+        if (c == undefined) { 
+        
+            if (group != "") { newString += "" + group.length + group[0] }
+            
+            break 
+        }
+    
+        string = string.substr(1)
+        
+        if (group == "") { group = c; continue }
+        
+        if (c == group[0]) { group += c; continue }
+        
+        newString += "" + group.length + group[0]
+        
+        group = c
+    }
+
+    string = newString
+}
+
+main()
+
