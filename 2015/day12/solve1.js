c84c9d5cdc32d76016b653f7ebe1f2fcda1945e1
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2023-09-24 09:15:20 -0300

    Add files via upload

---
+++ 2015/day12/solve1.js
@@ -0,0 +1,38 @@
+"use strict"
+
+// solving the puzzle takes (my computer) 0.025 s
+
+var total = 0
+
+function main() {
+
+    const source = Deno.readTextFileSync("input.txt").trim()
+    
+    const json = JSON.parse(source)
+    
+    processThis(json)
+
+    console.log("total is", total)
+}
+
+function processThis(thing) { // recursive function
+
+    if (typeof thing == "number") { total += thing }
+    
+    if (typeof thing != "object") { return }
+    
+    if (Array.isArray(thing)) { 
+        
+        for (const item of thing) { processThis(item) }
+        
+        return
+    }
+    
+    const values = Object.values(thing)   
+
+    const keys = Object.keys(thing)    
+    for (const key of keys) { processThis(thing[key]) }
+}
+
+main()
+
