6a0a5aa1548b5f1e1a238f1ad10ba6a16015ca66
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2023-10-10 12:29:16 -0300

    Add files via upload

--- 2017/day15/puzzle2.md
+++ 2017/day15/puzzle2.md
@@ -1,25 +1,59 @@
 ## --- Part Two ---
 
-Now, all the defragmenter needs to know is the number of **regions**. A region is a group of **used** squares that are all **adjacent**, not including diagonals. Every used square is in exactly one region: lone used squares form their own isolated regions, while several adjacent squares all count as a single region.
+In the interest of trying to align a little better, the generators get more picky about the numbers they actually give to the judge.
 
-In the example above, the following nine regions are visible, each marked with a distinct digit:
+They still generate values in the same way, but now they only hand a value to the judge when it meets their **criteria**:
+
+-   Generator A looks for values that are multiples of `**4**`.
+-   Generator B looks for values that are multiples of `**8**`.
+
+Each generator functions completely **independently**: they both go through values entirely on their own, only occasionally handing an acceptable value to the judge, and otherwise working through the same sequence of values as before until they find one.
+
+The judge still waits for each generator to provide it with a value before comparing them (using the same comparison method as before). It keeps track of the order it receives values; the first values from each generator are compared, then the second values from each generator, then the third values, and so on.
+
+Using the example starting values given above, the generators now produce the following first five values each:
 
 ```
-11.2.3..-->
-.1.2.3.4   
-....5.6.   
-7.8.55.9   
-.88.5...   
-88..5..8   
-.8...8..   
-88.8.88.-->
-|      |   
-V      V   
+--Gen. A--  --Gen. B--
+1352636452  1233683848
+1992081072   862516352
+ 530830436  1159784568
+1980017072  1616057672
+ 740335192   412269392
 ```
 
-Of particular interest is the region marked `8`; while it does not appear contiguous in this small view, all of the squares marked `8` are connected when considering the whole 128x128 grid. In total, in this example, `1242` regions are present.
+These values have the following corresponding binary values:
 
-**How many regions** are present given your key string?
+```
+01010000100111111001100000100100
+01001001100010001000010110001000
+
+01110110101111001011111010110000
+00110011011010001111010010000000
+
+00011111101000111101010001100100
+01000101001000001110100001111000
+
+01110110000001001010100110110000
+01100000010100110001010101001000
+
+00101100001000001001111001011000
+00011000100100101011101101010000
+```
+
+Unfortunately, even though this change makes more bits similar on average, none of these values' lowest 16 bits match. Now, it's not until the 1056th pair that the judge finds the first match:
+
+```
+--Gen. A--  --Gen. B--
+1023762912   896885216
+
+00111101000001010110000111100000
+00110101011101010110000111100000
+```
+
+This change makes the generators much slower, and the judge is getting impatient; it is now only willing to consider **5 million** pairs. (Using the values from the example above, after five million pairs, the judge would eventually find a total of `309` pairs that match in their lowest 16 bits.)
+
+After 5 million pairs, but using this new generator logic, **what is the judge's final count**?
 
 Although it hasn't changed, you can still [get your puzzle input](input.txt).
 
--- 2017/day15/solve2.js
+++ 2017/day15/solve2.js
@@ -1,202 +1,84 @@
 "use strict"
 
-// solving the puzzle takes (my computer) 0.210s
+// solving the puzzle takes (my computer) 5s
 
 
 function main() {
 
-    const keyString = Deno.readTextFileSync("input.txt").trim()
-    
-    const grid = [ ]
-            
-    for (let n = 0; n < 128; n++) {
-    
-        const thisKeyString = keyString + "-" + n
+    const rawText = Deno.readTextFileSync("input.txt").trim()
         
-        const hash = createKnotHash(thisKeyString)
-        
-         const row = createGridRow(hash)
-        
-        grid.push(row)
-    }
-
-    let region = 0
+    const rawLines = rawText.split("\n")
     
+    const tokensA = rawLines.shift().trim().split(" ")
+    
+    let generatorA = parseInt(tokensA.pop())
+    
+    const tokensB = rawLines.shift().trim().split(" ")
+    
+    let generatorB = parseInt(tokensB.pop())
+     
+    let counter = 0
+    
+    let matches = 0
+    
+    const max = 5 * 1000 * 1000
+        
     while (true) {
+        
+        generatorA = generateNewA(generatorA)
+        
+        const binaryA = generatorA.toString(2).padStart(16, 0)
+        
+        const bin16A = binaryA.substr(binaryA.length - 16)
+        
+        //
+        
+        generatorB = generateNewB(generatorB)  
 
-        const coord = findStartingCoordinates(grid)
+        const binaryB = generatorB.toString(2).padStart(16, 0)
         
-        if (coord == null) { break }
+        const bin16B = binaryB.substr(binaryB.length - 16) 
         
-        region += 1
+        //     
         
-        markRegion(grid, coord, region)
-    }
+        if (bin16A == bin16B) { matches += 1 }
+        
+        counter += 1
 
-    console.log("the number of regions is", region)
+        if (counter == max) { break }
+    }    
+    
+    console.log("after 5 million pairs, the count is", matches)
 }
 
-///////////////////////////////////////////////////////////
+function generateNewA(previous) {
 
-function createGridRow(hash) {
-
-    const row = [ ]
-    
-    for (const h of hash) { 
-        
-        const decimal = parseInt(h, 16)
-        
-        const binary = decimal.toString(2).padStart(4, "0")
-        
-        for (const b of binary) { row.push(b == "1" ? "-1" : "0") }            
-    }
-
-    return row
-}
-
-///////////////////////////////////////////////////////////
-
-function findStartingCoordinates(grid) {
-
-    for (let row = 0; row < 128; row++) {
-    
-        for (let col = 0; col < 128; col++) {
-        
-            if (grid[row][col] == -1) { return newCoordinates(row, col) }        
-        }    
-    }
-    return null
-}
-
-function markRegion(grid, coord, region) {
-
-    let futureCoords = [ coord ]
-    
     while (true) {
     
-        const currentCoords = futureCoords
-        
-        futureCoords = [ ]
-        
-        if (currentCoords.length == 0) { return }
-        
-        for (const coord of currentCoords) { walk(coord) }
-    }
+        const product = previous * 16807
 
-    function walk(coord) {
-    
-        const row = coord.row
-        const col = coord.col
-    
+        const result = product % 2147483647
         
-        grid[row][col] = region
+        if (result % 4 == 0) { return result }
         
-        tryWalk(row, col-1) // west
-        tryWalk(row, col+1) // east
-        tryWalk(row-1, col) // north
-        tryWalk(row+1, col) // south
-    }
-    
-    function tryWalk(row, col) {
-    
-        if (row < 0) { return }
-        if (col < 0) { return }
-        
-        if (row > 127) { return }
-        if (col > 127) { return }
-        
-        if (grid[row][col] != -1) { return } // empty or already walked
-        
-        futureCoords.push(newCoordinates(row, col))
+        previous = result
     }
 }
 
-///////////////////////////////////////////////////////////
+function generateNewB(previous) {
 
-function createKnotHash(keyString) {
-
-    const data = [ ]
+    while (true) {
     
-    for (let n = 0; n < 256; n++) { data.push(n) }
+        const product = previous * 48271
 
-    const lengths = [ ]
-    
-    for (const c of keyString) { const n = c.charCodeAt(0); lengths.push(n) }
-    
-    for (const n of [ 17, 31, 73, 47, 23 ]) { lengths.push(n) }
-
-    const info = { "data": data, "lengths": lengths, "currentPosition": 0, "skipSize": 0 }
-
-    for (let n = 0; n < 64; n++) { processOneRound(info) }
+        const result = product % 2147483647
         
-    //
-   
-    let hash = ""
-    
-    for (let n = 0; n < 16; n++) { 
-    
-        const sub = info.data.splice(0, 16) // sublist
+        if (result % 8 == 0) { return result }
         
-        const xor = sub[0] ^ sub[1] ^ sub[2] ^ sub[3] ^ sub[4] ^ sub[5] ^ sub[6] ^ sub[7] ^ 
-                    sub[8] ^ sub[9] ^ sub[10] ^ sub[11] ^ sub[12] ^ sub[13] ^ sub[14] ^ sub[15]
-        
-        const hexa = xor.toString(16).padStart(2, "0")
-        
-        hash += hexa
+        previous = result
     }
-
-    return hash
-}
-
-function processOneRound(info) {
-
-    const lengths = info.lengths.slice()
-    
-    while (lengths.length > 0) {
-
-        const length = lengths.shift()
-        
-        if (length > info.data.length) { continue }
-        
-        reverse(info.data, info.currentPosition, length)
-
-        info.currentPosition += length + info.skipSize        
-        
-        while (info.currentPosition >= info.data.length) { info.currentPosition -= info.data.length } // MUST BE while NOT if
-
-        info.skipSize += 1
-    }
-}
-    
-function reverse(data, start, length) {
-
-    const segment = [ ]
-
-    const end = start + length - 1
-    
-    for (let a = start; a <= end; a++) { 
-    
-        let index = a
-        if (index >= data.length) { index -= data.length }
-    
-        segment.push(data[index])
-    }
-    
-    for (let a = start; a <= end; a++) { 
-    
-        let index = a
-        if (index >= data.length) { index -= data.length }
-    
-        data[index] = segment.pop()
-    }
-}
-
-///////////////////////////////////////////////////////////
-
-function newCoordinates(row, col) {
-
-    return { "row": row, "col": col }
 }
 
 main()
+
 
