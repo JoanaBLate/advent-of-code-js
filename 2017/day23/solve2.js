26283e25dc5e7289c368a63b3bd5759aaeae3091
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2023-10-12 23:26:56 -0300

    Add files via upload

--- 2017/day23/solve2.js
+++ 2017/day23/solve2.js
@@ -1,126 +1,131 @@
 "use strict"
 
-// solving the puzzle takes (my computer) 0.040s
+// solving the puzzle takes (my computer) 0.320s
 
-const registers = { "a": 1 }
 
-const instructions = [ ]
+// THIS SOLUTION ONLY WORKS WITH THE CURRENT INPUT!!!
+//
+// this puzzle was made to stress your computer 
+// during many hours, maybe days using brute-force 
+// computing; we MUST change the assembly code, 
+// cleaning up the trash parts or running its 
+// essential code in other way
 
-var pointer = 0
 
-var shallStop = false
+var A = 1
+var B = 0
+var C = 0
+var D = 0
+var E = 0
+var F = 0
+var G = 0
+var H = 0
 
-var count = 0
+/*
+
+; F controls if H will be increased or not
+; G controls most of jumps
+
+01 set b 93      ; B = 93
+02 set c b       ; C = 93
+03 jnz a 2       ; GOTO 05; granted to jump 05 
+04 jnz 1 5       ; USELESS (unreachable line)
+05 mul b 100     ; B = 9300
+06 sub b -100000 ; B = 109300
+07 set c b       ; C = 109300
+08 sub c -17000  ; C = 126300
+
+09 set f 1       
+10 set d 2       ; second loop counter
+
+11 set e 2       ; first loop counter
+12 set g d      
+13 mul g e
+14 sub g b       ; G = D * E - B; when B == D * E -> F becomes 0 (line 16)
+15 jnz g 2       ; SKIP next line (G decides) 
+16 set f 0
+17 sub e -1      ; increase first loop counter
+18 set g e       ; prepare for jump check
+19 sub g b       ; prepare for jump check
+20 jnz g -8      ; GOTO 12 WHILE E != B
+
+21 sub d -1      ; increase second loop counter
+22 set g d       ; prepare for jump check
+23 sub g b       ; prepare for jump check
+24 jnz g -13     ; GOTO 11 WHILE D != B
+
+25 jnz f 2       ; SKIP next line (F decides)
+26 sub h -1      ; increase H
+27 set g b       ; prepare for jump check
+28 sub g c       ; prepare for jump check
+29 jnz g 2       ; SKIP next line WHILE B != C 
+30 jnz 1 3       ; END program
+31 sub b -17     ; add to B
+32 jnz 1 -23     ; GOTO 09; granted to jump
+
+
+*/
 
 
 function main() {
 
-    const rawText = Deno.readTextFileSync("input.txt").trim()
+    B = 109300
+    C = 126300
+
+    while (true) { // line 09
+    
+        F = 1
+        D = 2
         
-    const rawLines = rawText.split("\n")
-    
-    for (const rawLine of rawLines) { 
-    
-        const tokens = rawLine.trim().split(" ")
-    
-        instructions.push(tokens) 
+        while (true) { // line 11
+            
+            E = 2
+        
+            runFirstLoop()
+
+            D += 1
+            G = D - B
+            
+            if (G == 0) { break }
+        }
+        
+        if (F == 0) { H += 1 }
+        
+        G = B - C
+        
+        if (G == 0) { break }
+        
+        B += 17
     }
-
-    while (true) {
-    
-        if (pointer < 0  || pointer >= instructions.length) { break }
-
-        if (shallStop) { break }
-        
-        const tokens = instructions[pointer]
-        
-        const first = tokens[0]
-        
-        if (first == "mul") { mul(tokens.slice()); continue }
-        if (first == "jnz") { jnz(tokens.slice()); continue }
-        if (first == "set") { set(tokens.slice()); continue }
-        if (first == "sub") { sub(tokens.slice()); continue }
-    }    
-
-    console.log("value of register h is", registers["h"] )
+    console.log("the value left in register h is", H)
 }
 
-function mul(tokens) {
+function runFirstLoop() {
 
-    count += 1
-
-    tokens.shift() // mul
+    tryZeroF()
     
-    const name = tokens.shift()
-
-    maybeCreateRegister(name)
+    G = 0 // this loop ends when G is zero at line 20
+    E = B // E must equal B in order to make G zero
     
-    const value = getValue(tokens.shift())
-    
-    registers[name] *= value
-    
-    pointer += 1
 }
 
-function jnz(tokens) {
+function tryZeroF(){
+    
+    if (F == 0) { return }
 
-    tokens.shift() // jnz 
+    // if G is zero at line 15
+    // F is set to zero at line 16
+    // G being zero at that point implies that B = D * E 
+    // B and D doesn't change inside this loop, and E, ranges
+    // from 2 to B: searching  E == B / D
     
-    const value = getValue(tokens.shift())
-    
-    if (value == 0) { pointer += 1; return }
-    
-    const jumps = getValue(tokens.shift())
-    
-    pointer += jumps
-}
+    if (B % D != 0) { return } // E is an integer, not a float
 
-function set(tokens) { 
-
-    tokens.shift() // set
+    const quotient = B / D
     
-    const name = tokens.shift()
-
-    maybeCreateRegister(name)
+    if (quotient < 2  ||  quotient > B) { return } // out of E's range
     
-    const value = getValue(tokens.shift())
-    
-    registers[name] = value
-    
-    pointer += 1
-}
-
-function sub(tokens) { 
-
-    tokens.shift() // sub
-    
-    const name = tokens.shift()
-
-    maybeCreateRegister(name)
-    
-    const value = getValue(tokens.shift())
-    
-    registers[name] -= value
-    
-    pointer += 1
-}
-
-// helpers ////////////////////////////////////////////////
-
-function maybeCreateRegister(token) {
-    
-    if (registers[token] == undefined) { registers[token] = 0 }
-}
-
-function getValue(token) {
-
-    const number = parseInt(token)
-    
-    if (! isNaN(number)) { return number }
-
-    maybeCreateRegister(token)
-
-    return registers[token]
+    F = 0    
 }
 
 main()
