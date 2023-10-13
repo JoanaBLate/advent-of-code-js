a0942562a7f1e0a662775c90aff0e418f0f3b791
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2023-10-13 13:38:51 -0300

    Add files via upload

---
+++ 2017/day24/puzzle2.md
@@ -0,0 +1,15 @@
+## --- Part Two ---
+
+The bridge you've built isn't long enough; you can't jump the rest of the way.
+
+In the example above, there are two longest bridges:
+
+-   `0/2`\--`2/2`\--`2/3`\--`3/4`
+-   `0/2`\--`2/2`\--`2/3`\--`3/5`
+
+Of them, the one which uses the `3/5` component is stronger; its strength is `0+2 + 2+2 + 2+3 + 3+5 = **19**`.
+
+**What is the strength of the longest bridge you can make?** If you can make multiple bridges of the longest length, pick the **strongest** one.
+
+Although it hasn't changed, you can still [get your puzzle input](input.txt).
+
---
+++ 2017/day24/solve1.js
@@ -0,0 +1,187 @@
+"use strict"
+
+// solving the puzzle takes (my computer) 0.715s
+
+
+const TOKENS = [ ] // original input tokens
+
+var LENGTH = 0
+
+const strengths = { }  // token: value
+
+const matches = { } // end: [ tokens ]
+
+const layers = [ ] // bridges grabbed by length
+
+
+function main() {
+
+    processInput()
+    
+    fillStrengths()
+    
+    fillMatches()
+    
+    fillLayers()
+    
+    console.log("the strength of the strongest bridge is", findStrongest())
+}  
+
+///////////////////////////////////////////////////////////
+
+function processInput() {
+
+    const rawText = Deno.readTextFileSync("input.txt").trim()
+        
+    const rawLines = rawText.split("\n")
+    
+    for (const rawLine of rawLines) { TOKENS.push(rawLine.trim()) }
+         
+    LENGTH = TOKENS.length
+}
+
+function fillStrengths() {
+    
+    for (const token of TOKENS) {
+        
+        const parts = token.split("/")
+        
+        const endA = parseInt(parts.shift())
+        const endB   = parseInt(parts.shift())
+        
+        strengths[token] = endA + endB
+    }
+}
+
+function fillMatches() {
+
+    for (const token of TOKENS) {
+        
+        const parts = token.split("/")
+        
+        const endA = parts.shift()
+        const endB = parts.shift()
+        
+        if (matches[endA] == undefined) { matches[endA] = [ ] }
+
+        matches[endA].push(token)
+        
+        if (endA == endB) { continue }
+                
+        if (matches[endB] == undefined) { matches[endB] = [ ] }
+
+        matches[endB].push(token)
+    }
+}
+
+///////////////////////////////////////////////////////////
+    
+function fillLayers() {
+
+    let layer = createFirstLayer()
+
+    layers.push(layer)
+    
+    while (true) {
+
+        layer = createNewLayer(layer)
+        
+        if (layer.length == 0) { break }
+        
+        layers.push(layer)
+    }            
+}
+
+function createFirstLayer() {
+    
+    const layer = [ ]
+    
+    for (const token of TOKENS) {
+        
+        if (! token.startsWith("0/")) { continue }
+        
+        const parts = token.split("/")
+        
+        const bridge = { "tokens": [ token ], "end": parts.pop(), "strength": strengths[token] }
+        
+        layer.push(bridge)
+    }
+    
+    return layer
+}
+
+function createNewLayer(currentLayer) {
+
+    const newLayer = [ ]
+    
+    for (const bridge of currentLayer) { createAndPushNewBridges(bridge, newLayer) }
+    
+    return newLayer
+}
+
+function createAndPushNewBridges(bridge, layer) {
+    
+    const candidates = matches[bridge.end]
+    
+    for (const token of candidates) {
+    
+        if (bridge.tokens.includes(token)) { continue }
+        
+        const parts = token.split("/")
+        
+        const a = parts.shift()
+        const b = parts.shift()
+        
+        let newEnd = ""
+        
+        if (a == bridge.end) {
+        
+            newEnd = b
+        }
+        else if (b == bridge.end) {
+        
+            newEnd = a
+        }
+        
+        const newTokens = bridge.tokens.slice()
+        
+        newTokens.push(token)
+        
+        const newStrength = bridge.strength + strengths[token]
+        
+        const newBridge = { "tokens": newTokens, "end": newEnd, "strength": newStrength }
+        
+        layer.push(newBridge)
+    }
+}
+
+///////////////////////////////////////////////////////////
+
+function findStrongest() {
+
+    let best = 0
+    
+    for (const layer of layers) {
+    
+        const strength = getLayerStrength(layer)
+        
+        if (strength > best) { best = strength }
+    }
+    
+    return best
+}
+
+function getLayerStrength(layer) {
+
+    let best = 0
+    
+    for (const bridge of layer) {
+    
+        if (bridge.strength > best) { best = bridge.strength }
+    } 
+       
+    return best
+}
+
+main()
+
