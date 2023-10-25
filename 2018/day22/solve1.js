db2a56ae5ee5de803d65cfdd8a522e8768850ebf
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2023-10-25 11:19:25 -0300

    Add files via upload

---
+++ 2018/day22/solve1.js
@@ -0,0 +1,142 @@
+"use strict"
+
+// solving the puzzle takes (my computer) 0.030s
+
+var depth = 0
+
+var targetRow = 0
+var targetCol = 0
+
+var width = 0
+var height = 0
+
+const table = [ ] // rows of objects
+
+
+function main() {
+
+    processInput()
+     
+    makeTable()
+    
+    fillTable()
+    
+ // show()
+    
+    console.log("the answer is", calcRisk())
+}
+
+///////////////////////////////////////////////////////////
+
+function processInput() {
+
+    const input = Deno.readTextFileSync("input.txt").trim()
+        
+    const lines = input.split("\n")
+    
+    depth = parseInt(lines.shift().split(" ").pop())
+    
+    const tokens = lines.shift().replace("target:", "").split(",")
+    
+    targetCol = parseInt(tokens.shift())
+    targetRow = parseInt(tokens.shift())
+}
+
+function makeTable() {
+
+    width = targetCol + 1
+    height = targetRow + 1
+    
+    for (let row = 0; row < height; row++) { table.push(makeRow()) }
+    
+    function makeRow() {
+
+        const cells = [ ]    
+     
+        for (let col = 0; col < width; col++) { cells.push({ "geoIndex": 0, "erosion": 0 }) }
+        return cells
+    }
+}
+
+///////////////////////////////////////////////////////////
+
+function fillTable() {
+
+    for (let col = 0; col < width; col++) { 
+    
+        const cell = table[0][col]
+        
+        cell.geoIndex = col * 16807 
+        
+        cell.erosion = (cell.geoIndex + depth) % 20183       
+    }
+    
+    for (let row = 0; row < height; row++) { 
+    
+        const cell = table[row][0]
+
+        cell.geoIndex = row * 48271 
+        
+        cell.erosion = (cell.geoIndex + depth) % 20183
+    }
+    
+    for (let row = 1; row < height; row++) {
+
+        for (let col = 1; col < width; col++) { 
+        
+            const topCell = table[row-1][col]
+            
+            const leftCell = table[row][col-1]
+            
+            const cell = table[row][col]
+            
+            cell.geoIndex = topCell.erosion * leftCell.erosion
+            
+            cell.erosion = (cell.geoIndex + depth) % 20183
+        }
+    }
+    
+    const targetCell = table[targetRow][targetCol]
+    
+    targetCell.geoIndex = 0 
+        
+    targetCell.erosion = (targetCell.geoIndex + depth) % 20183
+}
+
+///////////////////////////////////////////////////////////
+
+function calcRisk() {
+
+    let risk = 0
+    
+   for (let row = 0; row < height; row++) {
+
+        for (let col = 0; col < width; col++) { 
+        
+            const cell = table[row][col]
+            
+            risk += cell.erosion % 3
+        }
+    }
+    
+    return risk
+}
+
+///////////////////////////////////////////////////////////
+
+function show() {
+
+    console.log("")
+    
+    for (const line of table) { 
+
+         let s = ""
+    
+        for (const cell of line) { s += ".=|"[cell.erosion % 3].padStart(3) }
+        
+        console.log(s) 
+    }
+}
+
+main()
+
