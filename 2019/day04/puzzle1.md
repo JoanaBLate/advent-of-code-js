b25e9770b8db500913f498184d2ca13eab9c27cf
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2023-11-03 07:29:59 -0300

    Add files via upload

---
+++ 2019/day04/puzzle1.md
@@ -0,0 +1,23 @@
+## --- Day 4: Secure Container ---
+
+You arrive at the Venus fuel depot only to discover it's protected by a password. The Elves had written the password on a sticky note, but someone threw it out.
+
+However, they do remember a few key facts about the password:
+
+*   It is a six-digit number.
+*   The value is within the range given in your puzzle input.
+*   Two adjacent digits are the same (like `22` in `1_22_345`).
+*   Going from left to right, the digits _never decrease_; they only ever increase or stay the same (like `111123` or `135679`).
+
+Other than the range rule, the following are true:
+
+*   `111111` meets these criteria (double `11`, never decreases).
+*   `2234`**`50`** does not meet these criteria (decreasing pair of digits `50`).
+*   `123789` does not meet these criteria (no double).
+
+**How many different passwords** within the range given in your puzzle input meet these criteria?
+
+Your puzzle input is `246515-739105`.
+
+To begin, [get your puzzle input](input.txt).
+
---
+++ 2019/day04/solve2.js
@@ -0,0 +1,56 @@
+"use strict"
+
+// solving the puzzle takes (my computer) 0.050s
+
+var lowerLimit = 0 
+var upperLimit = 0
+
+var passwords = 0
+
+
+function main() {
+
+    processInput()
+    
+    for (let n = lowerLimit; n <= upperLimit; n += 1) {
+    
+        const number = n.toString()
+        
+        let foundPair = false
+        let decreasing = false 
+        
+        for (let index = 1; index < 6; index++) {
+            
+            const current = number[index]
+            const previous = number[index - 1]
+            
+            if (current < previous) { decreasing = true; break }
+            
+            if (current == previous) { 
+            
+                if (index >= 2) { if (current == number[index - 2]) { continue } }
+                
+                if (index <= 4) { if (current == number[index + 1]) { continue } }
+            
+                foundPair = true                 
+            }
+        }
+        
+        if (foundPair  &&  ! decreasing) { passwords += 1 }    
+    }
+     
+    console.log("the answer is", passwords)
+}
+
+function processInput() {
+
+    const input = Deno.readTextFileSync("input.txt").trim()
+        
+    const tokens = input.split("-")
+    
+    lowerLimit = parseInt(tokens.shift())
+    upperLimit = parseInt(tokens.shift())
+}
+
+main()
+
