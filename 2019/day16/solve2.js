e20feb202fd3add8a9a779266c44442bcec559c0
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2023-11-09 20:29:36 -0300

    Add files via upload

--- 2019/day16/solve2.js
+++ 2019/day16/solve2.js
@@ -1,16 +1,36 @@
 "use strict"
 
-// solving the puzzle takes (my computer) 0.240s
+// solving the puzzle takes (my computer) 0.180s
 
+/* 
+    consider we have this input:
 
-// this code is heavilly based on the solution by Bimperl (https://www.reddit.com/user/Bimperl)
-// at https://www.reddit.com/r/adventofcode/comments/ebai4g/comment/fb5zy78
+    a b c d X f
 
-// this code expects that at least the first half of data be skipped
+    X is the position 5 (base one)
+
+    the pattern for X (position 5) is:   
+
+    0 0 0 0 1 1 (1 1 1 0 0 0 0 0 -1 -1 -1 -1 -1...)
+
+    HINTS:
+    
+    -> the pattern starts with 4 ZEROs: we can IGNORE everything
+    that comes before X (because it would be multiplied by 
+    zero and then added to the total value)
+
+    -> as X is close to the end, it and everything beyond will
+    be mutlipied by POSITIVE ONE: don't need to multiply
+    
+    -> we can have a very simplified, straight forward program!
+*/
+
+// this code expects that the target segment is on the second half of the input //
+
 
 const DATA = [ ]
 
-const repeats = 10000
+var repeats = 10000
 
 const maxPhases = 100
 
@@ -19,22 +39,30 @@ function main() {
 
     processInput()
 
-    const skip = parseInt(DATA.slice(0, 7).join(""))
+    let offset = parseInt(DATA.slice(0, 7).join(""))
     
-    let state = repeatArray(DATA, repeats).slice(skip)
+    while (offset >= DATA.length) { offset -= DATA.length; repeats -= 1 } // aborting useless data
+    
+    let state = repeatArray(DATA, repeats).slice(offset)
     
     let nextState = new Array(state.length)
             
     for (let n = 0; n < maxPhases; n++) { 
       
-        let count = 0
+        let sum = 0
         
-        for (let location = state.length - 1; location >= 0; location--) {
+        for (let index = state.length - 1; index >= 0; index--) { 
         
-            count = (count + state[location]) % 10
-            nextState[location] = count
+            // we just run the loop once, not once for each item of the state,
+            // imagine running 500_000 times a loop for 500_000 items
+            
+            // the loop must start at the end, because the sum increases backwards!
+        
+            sum += state[index]
+            
+            nextState[index] = sum % 10        
         }
-        
+
         [ state, nextState ] = [ nextState, state ]
     }
 
