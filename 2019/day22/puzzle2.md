1159280071826025bc6286e5decca60c5488c9db
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2023-11-19 08:03:15 -0300

    Add files via upload

---
+++ 2019/day22/puzzle2.md
@@ -0,0 +1,16 @@
+## --- Part Two ---
+
+After a while, you realize your shuffling skill won't improve much more with merely a single deck of cards. You ask every 3D printer on the ship to make you some more cards while you check on the ship repairs. While reviewing the work the droids have finished so far, you think you see [Halley's Comet](https://en.wikipedia.org/wiki/Halley%27s_Comet) fly past!
+
+When you get back, you discover that the 3D printers have combined their power to create for you a single, giant, brand new, **factory order** deck of **`119315717514047` space cards**.
+
+Finally, a deck of cards worthy of shuffling!
+
+You decide to apply your complete shuffle process (your puzzle input) to the deck **`101741582076661` times in a row**.
+
+You'll need to be careful, though - one wrong move with this many cards and you might **overflow** your entire ship!
+
+After shuffling your new, giant, **factory order** deck that many times, **what number is on the card that ends up in position `2020`?**
+
+Although it hasn't changed, you can still [get your puzzle input](input.txt).
+
---
+++ 2019/day22/solve1.js
@@ -0,0 +1,113 @@
+"use strict"
+
+// solving the puzzle takes (my computer) 0.030s
+
+const instructions = [ ]
+
+const LENGTH = 10007
+
+var deckA = new Uint16Array(LENGTH)
+var deckB = new Uint16Array(LENGTH)
+
+
+function main() {
+
+    processInput()
+    
+    for (let n = 0; n < LENGTH; n++) { deckA[n] = n }
+       
+    shuffle()
+    
+    for (let n = 0; n < LENGTH; n++) { 
+    
+        if (deckA[n] == 2019) { console.log("the answer is", n); return }
+    }
+}
+
+function processInput() {
+
+    const input = Deno.readTextFileSync("input.txt").trim()
+        
+    const lines = input.split("\n")
+    
+    for (const line of lines) { instructions.push(line.trim()) }
+}
+
+///////////////////////////////////////////////////////////
+
+function shuffle() {
+
+    for (const inst of instructions) {
+    
+        if (inst == "deal into new stack") { dealIntoNewStack(); continue }
+    
+        if (inst.startsWith("deal with increment")) { 
+        
+            const value = parseInt(inst.split(" ").pop())
+            
+            dealWithIncrement(value)
+            continue 
+        }
+        
+        if (inst.startsWith("cut")) { 
+        
+            const value = parseInt(inst.split(" ").pop())
+            
+            cut(value)
+            continue 
+        }
+    }
+}
+
+function exchangeDecks() {
+
+    const temp = deckA
+    
+    deckA = deckB
+    
+    deckB = temp
+}
+
+function dealIntoNewStack() { 
+
+    const last = LENGTH - 1
+    
+    for (let n = 0; n <= last; n++) { deckB[n] = deckA[last - n] }
+
+    exchangeDecks()
+}
+
+function cut(amount) {  
+
+    while (amount < 0) { amount += LENGTH }
+
+    const offset = LENGTH - amount
+    
+    for (let n = 0; n < LENGTH; n++) { 
+    
+        if (n < amount) { deckB[n + offset] = deckA[n] } else { deckB[n - amount] = deckA[n] }
+    }
+    
+    exchangeDecks()
+}
+
+function dealWithIncrement(increment) { 
+    
+    let previous = -increment
+   
+    for (let n = 0; n < LENGTH; n++) { 
+    
+        let index = previous + increment
+    
+        if (index >= LENGTH) { index -= LENGTH }
+    
+        deckB[index] = deckA[n]
+
+        previous = index
+    }
+
+    exchangeDecks()
+}
+
+main()
+
