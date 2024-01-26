72104dbbe4ed7db7c7505463ae97fe241a1db549
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2024-01-26 20:38:21 -0300

    Add files via upload

---
+++ 2021/day15/solve2.js
@@ -0,0 +1,196 @@
+"use strict"
+
+// solving the puzzle takes (my computer) 0.320s
+
+const input = Deno.readTextFileSync("input.txt").trim()
+
+const MAP = [ ]
+
+var HEIGHT = 0
+
+var WIDTH = 0
+
+const RISK = [ ]
+
+
+function main() {
+
+    processInput()
+    
+    expandMapHorizontally() 
+    
+    expandMapVertically()
+    
+    WIDTH *= 5
+    
+    HEIGHT *= 5
+        
+    createRiskTable()    
+    
+    walk()
+    
+    console.log("the answer is", RISK[HEIGHT - 1][WIDTH - 1])
+}
+
+///////////////////////////////////////////////////////////
+
+function processInput() {
+        
+    const lines = input.split("\n")
+    
+    for (const line of lines) { MAP.push(line.trim()) }
+    
+    HEIGHT = MAP.length
+    WIDTH = MAP[0].length
+}
+
+function createPoint(row, col) {
+
+    return { "row": row, "col": col }
+}
+
+///////////////////////////////////////////////////////////
+
+function expandMapHorizontally() {
+
+    for (let row = 0; row < HEIGHT; row++) {
+
+        MAP[row] = expandRowHorizontally(MAP[row])
+    }
+}
+
+function expandRowHorizontally(source) {
+
+    const list = source.split("").map(function (x) { return parseInt(x) })
+    
+    increaseList(list)
+    const a = list.join("")
+    
+    increaseList(list)
+    const b = list.join("")
+    
+    increaseList(list)
+    const c = list.join("")
+    
+    increaseList(list)
+    const d = list.join("")
+    
+    return source + a + b + c + d
+}
+
+///////////////////////////////////////////////////////////
+
+function expandMapVertically() { 
+
+    const block = [ ]
+    
+    for (const line of MAP) {
+    
+        const list = line.split("").map(function (x) { return parseInt(x) })
+        
+        block.push(list)
+    }
+    
+    increaseBlock(block)
+    pushBlock(block)
+    
+    increaseBlock(block)
+    pushBlock(block)
+    
+    increaseBlock(block)
+    pushBlock(block)
+    
+    increaseBlock(block)
+    pushBlock(block)    
+}
+
+function increaseBlock(block) {
+
+    for (const list of block) { increaseList(list) }
+}
+
+function pushBlock(block) {
+
+    for (const line of block) { MAP.push(line.join("")) }
+}
+
+///////////////////////////////////////////////////////////
+
+function increaseList(list) {
+
+    const off = list.length
+
+    for (let n = 0; n < off; n++) { 
+    
+        list[n] += 1
+        
+        if (list[n] == 10) { list[n] = 1 }
+    }
+}
+
+///////////////////////////////////////////////////////////
+
+function createRiskTable() {
+
+    for (let n = 0; n < HEIGHT; n++) { RISK.push(createRiskTableLine()) }
+}
+
+function createRiskTableLine() {
+
+    const list = new Array(WIDTH)
+    
+    list.fill(Infinity)
+    
+    return list
+}
+
+///////////////////////////////////////////////////////////
+
+function walk() {
+
+    RISK[0][1] = parseInt(MAP[0][1])
+    RISK[1][0] = parseInt(MAP[1][0])
+
+    let futures = [ createPoint(0, 1), createPoint(1, 0) ]
+        
+    while (true) {
+        
+        if (futures.length == 0) { return }
+    
+        const currents = futures
+        
+        futures = [ ]
+                
+        for (const current of currents) {
+        
+            const row = current.row
+            const col = current.col
+            
+            const risk = RISK[row][col]
+                        
+            tryWalk(row - 1, col, risk, futures)
+            tryWalk(row + 1, col, risk, futures)
+            tryWalk(row, col - 1, risk, futures)
+            tryWalk(row, col + 1, risk, futures)        
+        }        
+    }
+}
+    
+function tryWalk(row, col, risk, futures) {
+
+    if (row < 0) { return }
+    if (col < 0) { return }
+    if (row > HEIGHT - 1) { return }
+    if (col > WIDTH  - 1) { return }
+
+    const newRisk = risk + parseInt(MAP[row][col])
+    
+    if (newRisk >= RISK[row][col]) { return }
+    
+    RISK[row][col] = newRisk
+    
+    futures.push(createPoint(row, col))    
+}
+
+main()
+
