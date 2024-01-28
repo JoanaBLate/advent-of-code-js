9256cb35988facc3a2ff7d00078b74ed6d404a28
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2024-01-28 20:42:02 -0300

    Add files via upload

---
+++ 2021/day17/solve2.js
@@ -0,0 +1,152 @@
+"use strict"
+
+// solving the puzzle takes (my computer) 0.030s
+
+//                                                                     //
+// *WARNING* not expecting input with negative X or positive Y values  //
+//                                                                     //
+
+const input = Deno.readTextFileSync("input.txt").trim()
+
+var Xa = 0
+var Xb = 0
+var Ya = 0
+var Yb = 0
+
+const XSpeeds = [ ] 
+const YSpeeds = [ ]
+
+
+function main() {
+
+    processInput()
+    
+    fillXSpeeds()    
+    fillYSpeeds()
+    
+    console.log("the answer is", countValidCombinations())
+}
+
+///////////////////////////////////////////////////////////
+
+function processInput() {
+        
+    const parts = input.replace("target area: ", "").split(", ")
+    
+    const tokensX = parts.shift().substr(2).split("..")
+    const tokensY = parts.shift().substr(2).split("..")
+    
+    Xa = parseInt(tokensX.shift())
+    Xb = parseInt(tokensX.shift())
+    Ya = parseInt(tokensY.shift())
+    Yb = parseInt(tokensY.shift())
+    
+    if (Xa > Xb) { const temp = Xa; Xa = Xb; Xb = temp }
+    if (Ya < Yb) { const temp = Ya; Ya = Yb; Yb = temp } // for negative values
+}
+
+///////////////////////////////////////////////////////////
+
+function fillXSpeeds() {
+
+    for (let speed = 1; speed <= Xb; speed++) { tryXSpeed(speed) }
+}
+
+function tryXSpeed(speedAtStart) {
+
+    let speed = speedAtStart
+    
+    let x = 0
+
+    while (true) {
+    
+        x += speed
+        
+        if (x < Xa) { 
+        
+            speed -= 1 
+            
+            if (speed == 0) { return } else { continue }
+        }
+        
+        if (x <= Xb) { XSpeeds.push(speedAtStart) }
+        
+        return
+    }
+}
+
+///////////////////////////////////////////////////////////
+
+function fillYSpeeds() {
+
+    const minimumSpeed = Yb // negative speed
+    
+    for (let speed = minimumSpeed; speed < 0; speed++) { tryYSpeed(speed) } // negative speed
+    
+    // for positive ySpeed, may exist a gap in the middle of the range of good speeds //
+
+    const arbitraryMaxSpeed = 3 * Math.abs(Yb) // lacking some nice Math formula
+    
+    for (let speed = 0; speed <= arbitraryMaxSpeed; speed++) { tryYSpeed(speed) } // positive speed
+}
+
+function tryYSpeed(speedAtStart) {
+
+    let speed = speedAtStart
+    
+    let y = 0
+
+    while (true) {
+    
+        y += speed
+        
+        if (y > Ya) { speed -= 1 ; continue } // negative ys
+
+        if (y >= Yb) { YSpeeds.push(speedAtStart) }
+        
+        return 
+    }
+}
+
+///////////////////////////////////////////////////////////
+
+function countValidCombinations() {
+
+    let count = 0
+    
+    for (const speedX of XSpeeds) {
+    
+        for (const speedY of YSpeeds) { 
+        
+            if (gotMatch(speedX, speedY)) { count += 1 }
+        }
+    }
+    
+    return count
+}
+    
+function gotMatch(speedX, speedY) {
+
+    let x = 0
+    let y = 0
+
+    while (true) {
+
+        x += speedX
+        if (speedX > 0) { speedX -= 1 }
+        
+        y += speedY
+        speedY -= 1 
+        
+        if (x < Xa) { continue }
+        if (y > Ya) { continue }     // negative ys
+        
+        if (x > Xb) { return false }
+        if (y < Yb) { return false } // negative ys
+
+        return true
+    }
+}
+ 
+main()
+
