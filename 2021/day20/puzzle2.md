15c3aa7d1af03b85d661ec8089bbabf4c5afd1d9
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2024-02-01 17:50:13 -0300

    Add files via upload

---
+++ 2021/day20/puzzle2.md
@@ -0,0 +1,10 @@
+## --- Part Two ---
+
+You still can't quite make out the details in the image. Maybe you just didn't [enhance](https://en.wikipedia.org/wiki/Kernel_(image_processing)) it enough.
+
+If you enhance the starting input image in the above example a total of _50_ times, **`3351`** pixels are lit in the final output image.
+
+Start again with the original input image and apply the image enhancement algorithm 50 times. **How many pixels are lit in the resulting image?**
+
+Although it hasn't changed, you can still [get your puzzle input](input.txt).
+
---
+++ 2021/day20/solve2.js
@@ -0,0 +1,183 @@
+"use strict"
+
+// solving the puzzle takes (my computer) 0.180s
+
+const input = Deno.readTextFileSync("input.txt").trim()
+
+const algorithm = new Uint8Array(512)
+
+const DATA = [ ]
+
+const MARGIN = 52 
+
+var WIDTH = 0
+
+var HEIGHT = 0
+
+const DARK = 0
+const LIGHT = 1
+
+const MAPS = [ ]
+
+var anyDistantPixel = LIGHT
+
+
+function main() {
+
+    processInput()
+    
+    MAPS.push(createMapFromData())
+    MAPS.push(createBlankMap())
+    
+ // show(MAPS[0])
+
+    for (let n = 0; n < 50; n++) { updateMap() }    
+    
+    console.log("the answer is", countLight())
+}
+
+///////////////////////////////////////////////////////////
+
+function processInput() {
+        
+    const sections = input.split("\n\n")
+    
+    fillAlgorithm(sections.shift().trim())
+    
+    const lines = sections.shift().split("\n")
+    
+    for (const line of lines) { DATA.push(line.trim()) }
+    
+    HEIGHT = (2 * MARGIN) + DATA.length
+    
+    WIDTH =  (2 * MARGIN) + DATA[0].trim().length
+}
+
+///////////////////////////////////////////////////////////
+
+function fillAlgorithm(source) {
+
+    for (let n = 0; n < 512; n++) {
+        
+        if (source[n] == "#") { algorithm[n] = LIGHT }
+    }
+}
+
+///////////////////////////////////////////////////////////
+
+function createMapFromData() {
+
+    const map = createBlankMap()
+    
+    const height = HEIGHT - (2 * MARGIN)  
+    const width  = WIDTH  - (2 * MARGIN)   
+    
+    for (let row = 0; row < height; row++) { 
+
+        for (let col = 0; col < width; col++) { 
+    
+            if (DATA[row][col] == "#") { map[MARGIN + row][MARGIN + col] = LIGHT }
+        }
+    }
+    
+    return map
+}
+
+function createBlankMap() {
+ 
+    const map = [ ]   
+    
+    for (let n = 0; n < HEIGHT; n++) { map.push(new Uint8Array(WIDTH)) }
+    
+    return map
+} 
+
+///////////////////////////////////////////////////////////
+
+function updateMap() {
+
+    anyDistantPixel = (anyDistantPixel == DARK) ? LIGHT : DARK
+
+    const source = MAPS[0]
+    
+    const result = MAPS[1]
+    
+    MAPS.push(MAPS.shift())
+
+    for (let row = 0; row < HEIGHT; row++) { 
+
+        for (let col = 0; col < WIDTH; col++) { 
+    
+            result[row][col] = findResultFor(source, row, col) 
+        }
+    }
+}
+
+function findResultFor(map, row, col) {
+
+    let index = 0
+    
+    if (isLit(map, row - 1, col - 1)) { index += 256 }
+    if (isLit(map, row - 1, col    )) { index += 128 }
+    if (isLit(map, row - 1, col + 1)) { index += 64 }
+    
+    if (isLit(map, row    , col - 1)) { index += 32 }
+    if (isLit(map, row    , col    )) { index += 16 }
+    if (isLit(map, row    , col + 1)) { index += 8 }
+    
+    if (isLit(map, row + 1, col - 1)) { index += 4 }
+    if (isLit(map, row + 1, col    )) { index += 2 }
+    if (isLit(map, row + 1, col + 1)) { index += 1 }
+    
+    return algorithm[index]
+}
+
+function isLit(map, row, col) { 
+
+    if (row < 0) { return anyDistantPixel == LIGHT }
+    if (col < 0) { return anyDistantPixel == LIGHT }
+    
+    if (row > HEIGHT - 1) { return anyDistantPixel == LIGHT }
+    if (col > WIDTH - 1)  { return anyDistantPixel == LIGHT }
+    
+    return (map[row][col] == LIGHT)
+}
+
+///////////////////////////////////////////////////////////
+
+function countLight() {
+
+    const map = MAPS[0]
+
+    let count = 0
+
+    for (let row = 0; row < HEIGHT; row++) { 
+
+        for (let col = 0; col < WIDTH; col++) { 
+    
+            if (map[row][col] == LIGHT) { count += 1 }
+        }
+    }
+    
+    return count
+}
+
+///////////////////////////////////////////////////////////
+
+function show(map) {
+
+    console.log("") 
+    
+    for (const line of map) {
+    
+        let s = ""
+        
+        for (const c of line) { s += (c == DARK) ? "." : "#" }
+        
+        console.log(s)
+    } 
+    console.log("")    
+}
+
+main()
+
