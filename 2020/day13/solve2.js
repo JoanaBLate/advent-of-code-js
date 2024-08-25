ccb729be87d15b5ee861f7033028f80437cff3e0
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2024-08-25 20:47:57 -0300

    Update solve2.js

--- 2020/day13/solve2.js
+++ 2020/day13/solve2.js
@@ -1,71 +1,17 @@
+
 "use strict"
 
 // solving the puzzle takes (my computer) 0.025s
 
-/*
-    another puzzle that we cannot solve without using advanced math:
 
-    the Chinese Remainder Theorem 
+const buses = [ ]
 
-    so we are playing the Advent of **Math**, right?
     
-    this program was translated from a Python code on Reddit:
-    
-    >> SORRY FOR THE **CRAPPY** NAMES (bi, ni, Ni, bi, xi...)       
-*/
-
-const bi = [ ]
-
-const ni = [ ]
-
-
 function main() {
 
     processInput()
-    
-    let N = 1
-    
-    for (const n of ni) { N *= n }
-    
-    const Ni = [ ]
-    
-    for (const n of ni) { Ni.push(Math.floor(N / n)) }
-
-    const xi = []
-    
-    const product = []
-    
-    for (let n = 0; n < Ni.length; n++) {
-    
-        const num = Ni[n] % ni[n]
-        
-        let found = false
-        
-        let x = 1
-        
-        while (! found) {
-        
-            if ((x * num) % ni[n] == 1) {
-                found = true
-                break
-            }
-            x += 1
-        }
-        xi.push(x)
-    }
-
-    for (let n = 0; n < bi.length; n++) {
-    
-        product.push(BigInt(bi[n]) * BigInt(Ni[n]) * BigInt(xi[n]))
-    }   
-    
-    let sum = BigInt(0)
-    
-    for (const p of product) { sum += p }
-    
-    const result = sum % BigInt(N)
-    
-    console.log("the answer is", Number(result))    
+             
+    console.log("the answer is", search())
 }
 
 function processInput() {
@@ -74,21 +20,36 @@ function processInput() {
         
     const lines = input.split("\n")
     
-    lines.shift() // timestamp
-    
-    const tokens = lines.shift().trim().split(",")
-    
-    for (let n = 0; n < tokens.length; n++) {
-    
-        if (tokens[n] == "x") { continue }
+    const tokens = lines.pop().trim().split(",")
         
-        const bus = parseInt(tokens[n])
-    
-        bi.push((bus - n % bus) % bus)
-        
-        ni.push(bus)
+    for (const token of tokens) { 
+            
+        const bus = (token == "x") ? 1 : parseInt(token)
+
+        buses.push(bus)
     }
 }
 
-main()
+///////////////////////////////////////////////////////////////////////////////
 
+// this algorithm was copied from:
+// https://github.com/alvin-the-programmer/advent-of-code-2020/blob/main/walkthrough/d13/part2.js
+
+function search() {
+
+    let time = 0
+    let stepSize = buses[0]
+
+    for (let index = 1; index < buses.length; index++) {
+        
+        const bus = buses[index]
+
+        while ((time + index) % bus !== 0) { time += stepSize }
+
+        stepSize *= bus // need not to be LCM because all buses are prime numbers
+    }
+    
+    return time
+}
+
+main()
