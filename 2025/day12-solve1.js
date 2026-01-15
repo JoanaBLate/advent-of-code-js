0ea34c57aeba259b03e0cb5758c707c83fe448da
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2026-01-15 19:51:17 -0300

    Add files via upload

--- 2025/day12-solve1.js
+++ 2025/day12-solve1.js
@@ -43,33 +43,40 @@ function treeIsGood(rawLine) {
     const width  = parseInt(rawDimensions.shift())
     const height = parseInt(rawDimensions.shift())
     
-    const slots = [ ]
+    const amounts = [ ]
     
     const tokens = segments.shift().trim().split(" ")
     
-    for (const token of tokens) { slots.push(parseInt(token)) }
+    for (const token of tokens) { amounts.push(parseInt(token)) }
     
-    return treeIsGood2(width, height, slots)
+    return treeIsGood2(width, height, amounts)
 }
 
-function treeIsGood2(width, height, slots) {
+function treeIsGood2(width, height, amounts) {
+    
+    const loseWidth  = Math.floor(width / 3)
+    const loseHeight = Math.floor(height / 3)
+    
+    const capacityForLosePresents = loseWidth * loseHeight
+
+    let losePresents = 0
+    
+    for (const amount of amounts) { losePresents += amount }
+    
+    if (losePresents <= capacityForLosePresents) { return 1 }
+    
+    //
     
     const availableArea = width * height
     
     let tightPlacing = 0
     
-    let loosePlacing = 0
-    
     for (let index = 0; index < DENSITIES.length; index++) {
     
-        tightPlacing += slots[index] * DENSITIES[index] 
-        
-        loosePlacing += slots[index] * 9
+        tightPlacing += amounts[index] * DENSITIES[index]
     } 
     
     if (availableArea < tightPlacing) { return 0 }
-    
-    if (availableArea >= loosePlacing) { return 1 }
 
     console.log("ERROR: not expecting a complicated input"); Deno.exit()
 }
@@ -77,5 +84,4 @@ function treeIsGood2(width, height, slots) {
 console.time("execution time")
 main()
 console.timeEnd("execution time") // 3ms
-
 
